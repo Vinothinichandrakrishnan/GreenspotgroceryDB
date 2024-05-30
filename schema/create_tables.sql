a5fe3de44afaf00709f6da9dda0d08faa205e49a
Vinothini Chandrakrishnan <163810237+Vinothinichandrakrishnan@users.noreply.github.com>
2024-05-30 18:21:16 +0530

    Create create_tables.sql
    Query for creating the database and tables

---
+++ schema/create_tables.sql
@@ -0,0 +1,51 @@
+
+
+CREATE TABLE Vendors (
+    vendor_address VARCHAR(255) PRIMARY KEY
+);
+
+CREATE TABLE ItemTypes (
+    item_type VARCHAR(100) PRIMARY KEY,
+    item_type_name VARCHAR(100)
+);
+
+CREATE TABLE Locations (
+    location VARCHAR(100) PRIMARY KEY,
+    location_name VARCHAR(100)
+);
+
+CREATE TABLE Units (
+    unit VARCHAR(100) PRIMARY KEY,
+    unit_name VARCHAR(100)
+);
+
+CREATE TABLE Items (
+    item_num INT PRIMARY KEY,
+    description VARCHAR(255),
+    quantity_on_hand INT,
+    cost DECIMAL(10, 2),
+    purchase_date DATE,
+    vendor_address VARCHAR(255),
+    price DECIMAL(10, 2),
+    item_type VARCHAR(100),
+    location VARCHAR(100),
+    unit VARCHAR(100),
+    FOREIGN KEY (vendor_address) REFERENCES Vendors(vendor_address),
+    FOREIGN KEY (item_type) REFERENCES ItemTypes(item_type),
+    FOREIGN KEY (location) REFERENCES Locations(location),
+    FOREIGN KEY (unit) REFERENCES Units(unit)
+);
+
+CREATE TABLE Sales (
+    sale_id INT PRIMARY KEY AUTO_INCREMENT,
+    date_sold DATE,
+    item_num INT,
+    cust_id INT,
+    quantity INT,
+    FOREIGN KEY (item_num) REFERENCES Items(item_num)
+);
+
+
+
+
+
