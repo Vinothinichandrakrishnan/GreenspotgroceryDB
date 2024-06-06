de1d3869c7c6e4f8b4c2e2fdee9db3467e7ed24c
Vinothini Chandrakrishnan <163810237+Vinothinichandrakrishnan@users.noreply.github.com>
2024-06-06 11:31:34 +0530

    Update business_queries.sql

--- queries/business_queries.sql
+++ queries/business_queries.sql
@@ -10,7 +10,7 @@ JOIN Grocery_data.Items ON Sales.item_id = Items.item_id
 GROUP BY Items.item_id, Items.description, Items.QTY_OH
 HAVING Items.QTY_OH < SUM(Sales.quantity);
 
--- Query to find the total spent by each customers
+-- Query to find the total spent by each customer
 
 SELECT 
 Items.item_id,
@@ -19,3 +19,44 @@ SUM(Items.price * Sales.quantity) AS total_spent
 FROM Grocery_data.Sales
 JOIN Grocery_data.Items ON Items.item_id = Sales.item_id
 GROUP BY Items.item_id, Sales.cust_id;
+
+-- Query maximum total sales of the item 
+
+SELECT 
+Sales.item_num,
+description,
+sum(Sales.quantity) AS total_sales
+FROM Grocery_data.Items
+JOIN Grocery_data.Sales ON Sales.item_num = Items.item_num
+GROUP BY Sales.item_num,
+description
+ORDER By total_sales DESC
+LIMIT 1;
+
+-- Query total sales per item
+
+SELECT 
+Sales.item_num,
+description,
+SUM(Sales.quantity) AS total_sales
+FROM Grocery_data.Items
+JOIN Grocery_data.Sales ON Sales.item_num = Items.item_num
+GROUP BY Sales.item_num,
+description;
+
+-- Query to find total sales for an item having "item_num = 1000" 
+
+SELECT 
+Sales.item_id,
+Items.item_num,
+description,
+Sales.date_sold,
+SUM(Sales.quantity) AS total_sales
+FROM Grocery_data.Items
+JOIN Grocery_data.Sales ON Sales.item_id = Items.item_id
+WHERE Items.item_num = 1000
+GROUP BY Sales.item_id,
+Items.item_num,
+description,
+Sales.date_sold
+HAVING total_sales;
